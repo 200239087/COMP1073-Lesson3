6c4269a17bb6a0cedf5f78bf7d9acc1b9446b25f
200239087 <200239087@student.georgianc.on.ca>
2017-01-25 14:12:50 -0500

    Added JavaScript

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,6 +2,30 @@
 
 console.log("App Started");
 
+// Declare or initialize variable
+// variable creats a link to the H1 element
+var firstHeading = document.getElementById("firstHeading");
+
+// Do not use window.alert or document.write
+console.log(firstHeading.textContent);
+
+//Three Steps for injecting content
+// Step 1 - create a reference to an element
+var FirstParagraph = document.getElementById("FirstParagraph");
+
+// Step 2 - create a variable that contains your content
+var myContent = "Changed Content!";
+
+// Step 3 - assign the variable with your content to the  textContent property of the reference variable
+FirstParagraph.textContent = myContent;
+
+var SecondParagraph = document.getElementById("SecondParagraph");
+
+var myHTMLContent = "<h2>Second Heading</h2>"
+                    +" <p>This is an inner paragraph to the Second Paragraph</p>";
+
+SecondParagraph.innerHTML = myHTMLContent;
+
 // create a reference to the button on the page (index.html)
 var clickMeButton = document.getElementById("clickMeButton");
 
